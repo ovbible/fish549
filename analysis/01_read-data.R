6f90b7e07f0c5846ff13b089ce08158c70514796
Olivia Bible <ovbible@bu.edu>
2025-01-24 14:36:15 -0800

    added read-data script

---
+++ analysis/01_read-data.R
@@ -0,0 +1,14 @@
+## This script will read in the raw data
+
+##installing package
+install.packages("here")
+
+## load libraries
+library(here)
+
+## set data directory
+data_dir <- here("data")
+
+
+
+
